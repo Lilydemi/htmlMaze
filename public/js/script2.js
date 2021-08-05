853a0aa7741e19a975b510e5ab9d4707d6bc79d3
Lidya Demilew <si.demilew@gmail.com>
2021-08-05 15:41:14 +0000

    challanges appear now

--- public/js/script2.js
+++ public/js/script2.js
@@ -401,6 +401,7 @@ function onKeyDown(event) {
             alert("You made it to the end of the maze. Congrats! Your time was " + minutes +" minutes and " + seconds + " seconds"); 
         }, 500);
        stopTimer();
+       alert("Please enter your namet to save your time!");
     } 
 }
 
@@ -413,7 +414,7 @@ function onLoad() {
   progressBar = document.querySelector('#progressBar');  
 
   player = new Player();
-  maze = new Maze(20, 20, 50);
+  maze = new Maze(5, 5, 20);
 
   document.addEventListener('keydown', onKeyDown);
   //document.getElementById('generate').addEventListener('click', onClick);
