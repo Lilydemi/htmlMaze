d223bea5773d35ae71b53a30795a2eaf99aba287
Huarui Lai <huarui.lai@gmail.com>
2021-08-05 16:27:22 +0000

    bru

--- public/js/script2.js
+++ public/js/script2.js
@@ -9,6 +9,7 @@ var count = 0;
 let oneCh = false;
 let twoCh = false;
 let thrCh = false;
+var challengeCount = 0;
 
 
 //let playerIcon = new Image();
@@ -256,6 +257,7 @@ class Maze {
                     oneCh = true;
                     console.log("oneCh: " + oneCh);
                     this.challengeColorOne = "#FFFFFF";
+                    challengeCount++;
                     //myFunction1();
             }
             else if(player.col==this.challengeTwoCol && 
@@ -263,6 +265,7 @@ class Maze {
                     twoCh = true;
                     console.log("TwoCh: " + twoCh);
                     this.challengeColorTwo = "#FFFFFF";
+                    challengeCount++;
                     //myFunction2();
    
                 }
@@ -271,6 +274,7 @@ class Maze {
                     thrCh = true;
                     console.log("threeCh: " + thrCh);
                     this.challengeColorThree = "#FFFFFF";
+                    challengeCount++;
                     //myFunction3();
             }
 
